2a8c9c093e3b5e217f39b3f8b9826c8582d3a1b0
Aditya Eka <aditya.eka409@gmail.com>
2019-05-01 11:37:53 +0700

    first

---
+++ nodemcu-esp8266/nodemcu-esp8266.ino
@@ -0,0 +1,83 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// const char* ssid = "TP-Link_D0A85";
+const char* ssid = "Incognito";
+const char* pass = "leomessi";
+
+const char* mqttServer = "149.56.36.129"; 
+const int mqttPort = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(0, OUTPUT);
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, pass);
+
+    Serial.print("Connecting");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(50);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("Wi-Fi Connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    client.setServer(mqttServer, mqttPort);
+    client.setCallback(MQTTcallback);
+    while (!client.connected()) {
+        Serial.println("Connecting to MQTT");
+        if (client.connect("ESP8266")) {
+            Serial.println("Connected");
+        } else {
+            Serial.print("Failed with state");
+            Serial.println(client.state());
+            delay(2000);
+        }
+    }
+    client.publish("lampu", "Halo! saya ESP8266");
+    client.subscribe("lampu");
+}
+
+void MQTTcallback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Topic: ");
+    Serial.println(topic);
+    
+    Serial.print("Pesan: ");
+
+    String message;
+    for (int i = 0; i<length; i++) {
+        message = message + (char)payload[i];
+    }
+    Serial.print(message);
+    
+    if (message == "#on_1") {
+        digitalWrite(5, HIGH);
+    } else if (message == "#on_2") {
+        digitalWrite(4, HIGH);
+    } else if (message == "#on_3") {
+        digitalWrite(0, HIGH);
+    }
+    
+    if (message == "#off_1") {
+        digitalWrite(5, LOW);
+    } else if (message == "#off_2") {
+        digitalWrite(4, LOW);
+    } else if (message == "#off_3") {
+        digitalWrite(0, LOW);
+    }
+
+    Serial.println();
+    Serial.println("---------------");
+}
+
+void loop() {
+    client.loop();
+}
---
+++ websocket/assets/js/mqttconfig.js
@@ -0,0 +1,46 @@
+let broker = {
+    host: '149.56.36.129',
+    port: 18083,
+    clientId: 'web_' + parseInt(Math.random()*1000,10)
+}
+let client = new Paho.MQTT.Client(broker.host, Number(broker.port), broker.clientId)
+
+client.onConnectionLost = onConnectionLost
+client.onMessageArrived = onMessageArrived
+
+let options = {
+    onSuccess:onConnect
+}
+
+client.connect(options)            
+function onConnect() {
+    console.log("Koneksi berhasil!")
+    document.getElementById("log").innerHTML += "Koneksi berhasil!<br>"
+    client.subscribe("lampu")
+    message = new Paho.MQTT.Message("Halo! saya " + broker.clientId)
+    message.destinationName ="lampu"
+    client.send(message)
+}
+
+function ledState(no, state) {
+    if (state == 1) {
+        message = new Paho.MQTT.Message("#on" + no)
+    }
+    if (state == 0) {
+        message = new Paho.MQTT.Message("#off" + no)
+    }
+    message.destinationName ="lampu"
+    client.send(message)
+}
+
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("Koneksi gagal: "+responseObject.errorMessage)
+        document.getElementById("log").innerHTML += "Koneksi gagal: "+responseObject.errorMessage+"<br>"
+    }
+}
+
+function onMessageArrived(message) {
+    console.log("Pesan baru: "+message.payloadString)
+    document.getElementById("log").innerHTML += "Pesan baru: "+message.payloadString+"<br>"
+}
